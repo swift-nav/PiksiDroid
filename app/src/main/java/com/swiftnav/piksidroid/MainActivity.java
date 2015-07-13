8be73d00d2fcc1eab0abcd67d624e88c34c7602f
Vlad Ungureanu <vvu@vdev.ro>
2015-07-13 14:14:25 -0700

    Enumerate usb devs.
    Signed-off-by: Vlad Ungureanu <vvu@vdev.ro>

--- app/src/main/java/com/swiftnav/piksidroid/MainActivity.java
+++ app/src/main/java/com/swiftnav/piksidroid/MainActivity.java
@@ -1,17 +1,42 @@
 package com.swiftnav.piksidroid;
 
-import android.support.v7.app.ActionBarActivity;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
 import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.HashMap;
+import java.util.Iterator;
+
 
 public class MainActivity extends ActionBarActivity {
+	public String TAG = "PiksiDroid";
+	private static final String ACTION_USB_PERMISSION =
+			"com.android.example.USB_PERMISSION";
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		UsbManager mUsbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+		PendingIntent mPermissionIntent = PendingIntent.getBroadcast(this, 0, new Intent(ACTION_USB_PERMISSION), 0);
+		IntentFilter filter = new IntentFilter(ACTION_USB_PERMISSION);
+		registerReceiver(mUsbReceiver, filter);
+
+		HashMap<String, UsbDevice> deviceList = mUsbManager.getDeviceList();
+		Iterator<UsbDevice> deviceIterator = deviceList.values().iterator();
+		while(deviceIterator.hasNext()){
+			UsbDevice device = deviceIterator.next();
+			Log.d(TAG, device.getProductName());
+		}
 	}
 
 	@Override
@@ -35,4 +60,25 @@ public class MainActivity extends ActionBarActivity {
 
 		return super.onOptionsItemSelected(item);
 	}
+
+	private final BroadcastReceiver mUsbReceiver = new BroadcastReceiver() {
+
+		public void onReceive(Context context, Intent intent) {
+			String action = intent.getAction();
+			if (ACTION_USB_PERMISSION.equals(action)) {
+				synchronized (this) {
+					UsbDevice device = (UsbDevice)intent.getParcelableExtra(UsbManager.EXTRA_DEVICE);
+
+					if (intent.getBooleanExtra(UsbManager.EXTRA_PERMISSION_GRANTED, false)) {
+						if(device != null){
+							Log.d(TAG, "We have OK!");
+						}
+					}
+					else {
+						Log.d(TAG, "permission denied for device " + device);
+					}
+				}
+			}
+		}
+	};
 }
